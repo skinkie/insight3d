70a543514ef72536a4f1e0f34a9d77f151ea199e
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2020-03-02 13:41:10 +0100

    Remove highgui includes

--- insight3d/interface_opencv.h
+++ insight3d/interface_opencv.h
@@ -24,8 +24,8 @@
 #include "pthread.h"
 #include "stdio.h"
 #include <opencv2/core/core_c.h>
-#include <opencv2/highgui/highgui_c.h>
 #include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
 
 const double OPENCV_PI = 3.14159265358979323846;
 
