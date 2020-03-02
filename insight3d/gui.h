d1e3f0500d490fd2441fba6aeac49acfe695508d
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2020-03-02 13:41:10 +0100

    Remove highgui includes

--- insight3d/gui.h
+++ insight3d/gui.h
@@ -6,7 +6,8 @@
 #include "SDL_opengl.h"
 #include <math.h>
 #include <opencv2/core/core_c.h>
-#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
 
 #ifndef SIZE_MAX
 #define SIZE_MAX ((size_t)-1)
